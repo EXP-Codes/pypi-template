29d8bd6d44b65138ac7a7df9771609df8cc0f8d7
EXP <289065406@qq.com>
2022-02-02 17:39:58 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -129,7 +129,7 @@ setup(
     #
     #   py_modules=["my_module"],
     #
-    packages=find_packages(where=".", exclude=["tests"]),  # Required
+    packages=find_packages(where=".", exclude=["test"]),  # Required
 
     # Specify which Python versions you support. In contrast to the
     # 'Programming Language' classifiers above, 'pip install' will check this
